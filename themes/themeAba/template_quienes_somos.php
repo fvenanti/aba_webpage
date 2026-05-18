3ee374766dc8afde1a780e0762f0395f9608e7ce
fvenanti <fvenanti@gmail.com>
2026-05-18 12:16:51 -0300

    Quienes Somos: two-column flexbox layout, image left text right

--- themes/themeAba/template_quienes_somos.php
+++ themes/themeAba/template_quienes_somos.php
@@ -13,12 +13,16 @@
 
 					<!-- article -->
 					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-						<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
-							<?php the_post_thumbnail(array(500,250), array('class' => 'left blockPhone')); // Declare pixel size you need inside the array ?>
-						<?php endif; ?>
-						<?php the_content(); ?>
-
-						<br class="clear">
+						<div style="display:flex;gap:48px;align-items:flex-start;flex-wrap:wrap;">
+							<?php if ( has_post_thumbnail()) : ?>
+							<div style="flex:0 0 380px;max-width:100%;">
+								<?php the_post_thumbnail(array(500,400), array('style' => 'width:100%;height:auto;border-radius:8px;')); ?>
+							</div>
+							<?php endif; ?>
+							<div style="flex:1;min-width:260px;">
+								<?php the_content(); ?>
+							</div>
+						</div>
 
 						<?php edit_post_link(); ?>
 
